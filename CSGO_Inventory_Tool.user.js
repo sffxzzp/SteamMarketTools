244f8fc880626a64f108d4e42d28746e0437b45c
sffxzzp@126.com <sffxzzp@126.com>
2018-10-17 00:38:42 +0800

    change metjm.net to csgofloat.com

--- CSGO_Inventory_Tool.user.js
+++ CSGO_Inventory_Tool.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         CSGO Inventory Float Value
 // @namespace    https://coding.net/u/sffxzzp
-// @version      0.02
+// @version      0.03
 // @description  A script that displays float value & screenshot of csgo skins
 // @author       sffxzzp
 // @match        *://steamcommunity.com/*/inventory*
 // @icon         https://steamcommunity.com/favicon.ico
 // @grant        GM_xmlhttpRequest
-// @connect      metjm.net
+// @connect      api.csgofloat.com
 // @updateURL    https://coding.net/u/sffxzzp/p/CSGO-Market-Tool/git/raw/master/CSGO_Inventory_Tool.user.js
 // ==/UserScript==
 
@@ -54,63 +54,26 @@
                 }
             });
         };
-        util.wrun = function (data) {
-            setTimeout(data.run||null, data.ms);
-        };
-        util.guid = function () {
-            function S4() {
-                return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-            }
-            return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-        };
         return util;
     })();
     var ifv = (function () {
         function ifv() {}
-        ifv.prototype.getFloatValue = function (node, guid, itemId) {
+        ifv.prototype.getFloatValue = function (node) {
             let _this = this;
-            if (itemId=="first") {
-                util.xhr({
-                    url: "http://metjm.net/shared/screenshots-v5.php?cmd=request_new_link&user_uuid="+guid+"&user_client=1&custom_rotation_id=0&use_logo=0&mode=7&resolution=2&forceOpskins=0&inspect_link="+encodeURIComponent(node.getAttribute("link"))
-                }).then(function (result) {
-                    result = JSON.parse(result.body);
-                    if (result.success===true) {
-                        node.onclick = function () {};
-                        _this.getFloatValue(node, guid, result.result.screen_id);
-                    }
-                    else if (result.success===false) {
-                        node.innerHTML = "<span>查询失败</span>";
-                    }
-                });
-            }
-            else {
-                util.xhr({
-                    url: "http://metjm.net/shared/screenshots-v5.php?cmd=request_screenshot_status&id="+itemId,
-                }).then(function (result) {
-                    result = JSON.parse(result.body);
-                    if (result.success===true) {
-                        if (result.result.status==1) {
-                            if (result.result.item_floatvalue===0) {
-                                node.innerHTML = "<span>队列中：第"+result.result.place_in_queue+"位</span>";
-                            }
-                            else if (result.result.item_floatvalue > 0) {
-                                node.innerHTML = "<span>"+result.result.item_floatvalue+"</span>";
-                            }
-                            util.wrun({ run: function () { _this.getFloatValue(node, guid, itemId); }, ms: 10000});
-                        }
-                        else if (result.result.status==2) {
-                            node.innerHTML = "<span>"+result.result.item_floatvalue+"</span>";
-                            node.className="btn_green_white_innerfade btn_small";
-                            node.onclick = function() {
-                                window.open(result.result.image_url);
-                            };
-                        }
-                    }
-                    else if (result.success===false) {
-                        node.innerHTML = "查询失败";
-                    }
-                });
-            }
+            util.xhr({
+                url: "https://api.csgofloat.com/?url="+node.getAttribute("link")
+            }).then(function (result) {
+                result = JSON.parse(result.body);
+                if (result.iteminfo) {
+                    node.onclick = function () {};
+                    node.innerHTML = "<span>"+result.iteminfo.floatvalue.toFixed(14)+"</span>";
+                    node.className="btn_green_white_innerfade btn_small";
+                    localStorage.setItem(node.id, JSON.stringify({fv:result.iteminfo.floatvalue.toFixed(14)}));
+                }
+                else {
+                    node.innerHTML = "<span>查询失败</span>";
+                }
+            });
         };
         ifv.prototype.Start = function () {
             let _this = this;
@@ -126,7 +89,7 @@
                             fvbtn.innerHTML = "<span>点击查询磨损</span>";
                             fvbtn.onclick = function () {
                                 this.innerHTML = "<span>磨损查询中…</span>";
-                                _this.getFloatValue(this, util.guid(), "first");
+                                _this.getFloatValue(this);
                             };
                             rec.target.appendChild(fvbtn);
                         }
